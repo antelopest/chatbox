f4b5ba6f5dc0386a909884316952f5b51a4205ff
antelopest <antelopest@gmail.com>
2026-02-09 16:55:03 +0700

    main add controller for messages module

--- apps/api/src/messages/messages.module.ts
+++ apps/api/src/messages/messages.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { MessagesController } from './api/messages/messages.controller';
 
-@Module({})
+@Module({
+  controllers: [MessagesController],
+})
 export class MessagesModule {}
